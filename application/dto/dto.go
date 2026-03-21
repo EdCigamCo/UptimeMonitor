5915d322dd265f6238ef10caf7438e625c0f3f40
Maya Pimenova <majapimenova@gmail.com>
2026-03-21 23:00:08 +0300

    TS-6: add API endpoint for check history

--- application/dto/dto.go
+++ application/dto/dto.go
@@ -7,9 +7,12 @@ type CreateSiteRequest struct {
 
 // SiteResponse represents a site in API response
 type SiteResponse struct {
-	ID        int64  `json:"id"`
-	URL       string `json:"url"`
-	CreatedAt string `json:"created_at"`
+	ID           int64  `json:"id"`
+	URL          string `json:"url"`
+	CreatedAt    string `json:"created_at"`
+	Status       string `json:"status,omitempty"`
+	ResponseTime int64  `json:"response_time,omitempty"`
+	LastChecked  string `json:"last_checked,omitempty"`
 }
 
 // SiteListResponse represents list of sites in API response
@@ -20,4 +23,19 @@ type SiteListResponse struct {
 // ErrorResponse represents error in API response
 type ErrorResponse struct {
 	Error string `json:"error"`
+}
+
+// CheckResponse represents a single check in API response
+type CheckResponse struct {
+	ID           int64  `json:"id"`
+	Status       string `json:"status"`        // "up" or "down"
+	ResponseTime int64  `json:"response_time"` // in milliseconds
+	CheckedAt    string `json:"checked_at"`    // RFC3339 format
+}
+
+// SiteHistoryResponse represents check history for a site
+type SiteHistoryResponse struct {
+	SiteID int64           `json:"site_id"`
+	URL    string          `json:"url"`
+	Checks []CheckResponse `json:"checks"`
 }
--- presentation/mapper/mapper.go
+++ presentation/mapper/mapper.go
@@ -25,3 +25,60 @@ func ToSiteListResponse(sites []model.Site) dto.SiteListResponse {
 		Sites: responses,
 	}
 }
+
+// ToCheckResponse converts model.Check to dto.CheckResponse
+func ToCheckResponse(check *model.Check) dto.CheckResponse {
+	return dto.CheckResponse{
+		ID:           check.ID,
+		Status:       check.Status,
+		ResponseTime: check.ResponseTime,
+		CheckedAt:    check.CheckedAt.Format(time.RFC3339),
+	}
+}
+
+// ToSiteHistoryResponse converts model.Site and []model.Check to dto.SiteHistoryResponse
+func ToSiteHistoryResponse(site *model.Site, checks []model.Check) dto.SiteHistoryResponse {
+	checkResponses := make([]dto.CheckResponse, 0, len(checks))
+	for i := range checks {
+		checkResponses = append(checkResponses, ToCheckResponse(&checks[i]))
+	}
+
+	return dto.SiteHistoryResponse{
+		SiteID: site.ID,
+		URL:    site.URL,
+		Checks: checkResponses,
+	}
+}
+
+// ToSiteResponseWithCheck converts model.Site and optional model.Check to dto.SiteResponse
+func ToSiteResponseWithCheck(site *model.Site, check *model.Check) dto.SiteResponse {
+	response := dto.SiteResponse{
+		ID:        site.ID,
+		URL:       site.URL,
+		CreatedAt: site.CreatedAt.Format(time.RFC3339),
+	}
+
+	if check != nil {
+		response.Status = check.Status
+		response.ResponseTime = check.ResponseTime
+		response.LastChecked = check.CheckedAt.Format(time.RFC3339)
+	}
+
+	return response
+}
+
+// ToSiteListResponseWithChecks converts []model.Site and []*model.Check to dto.SiteListResponse
+func ToSiteListResponseWithChecks(sites []model.Site, checks []*model.Check) dto.SiteListResponse {
+	responses := make([]dto.SiteResponse, 0, len(sites))
+	for i, site := range sites {
+		var check *model.Check
+		if i < len(checks) {
+			check = checks[i]
+		}
+		responses = append(responses, ToSiteResponseWithCheck(&site, check))
+	}
+
+	return dto.SiteListResponse{
+		Sites: responses,
+	}
+}
